e5e2b37fe5ba1a4767ae901e3ae0a2f8e5b0ace4
Heorhij <118767059+gshaa@users.noreply.github.com>
2023-02-13 17:34:54 +0100

    made responsive version

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,40 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
+//nav bar
+
+$(document).ready(function () {
+  $("#icon").click(function () {
+    $("ul").toggleClass("show");
+  });
+});
+
+$(document).ready(function () {
+  $(".mobile-menu").click(function () {
+    $("ul").removeClass("show");
+  });
+});
+
+const logoToggleOverflow = () => {
+  if (window.innerWidth < 1040) {
+    document.body.style.overflow = document.body.style.overflow = "auto";
+    $("ul").removeClass("show");
   }
-}
+};
+
+const toggleOverflow = () => {
+  if (window.innerWidth < 1040) {
+    document.body.style.overflow =
+      document.body.style.overflow === "hidden" ? "auto" : "hidden";
+  }
+};
+
+// animation
+
+const links = document.querySelectorAll("nav a");
+
+links.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const target = document.querySelector(link.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
